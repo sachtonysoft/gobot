8b8766d7011156108c4ca7477a0d4366e8a9c5f0
Sachtony Company Limited <51844180+sachtonysoft@users.noreply.github.com>
2019-12-28 12:27:25 +0700

    Add files via upload

---
+++ CallBack.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"master"
+	
+	"Sachtony"
+	"Sachtonysoft"
+	"SachtonyStore"
+	"TonyDuong"
+	"Merchants"
+	"AdminCenter"
+	"PartnerCenter"
+	"Developers"
+	
+)
+
+func main() {
+
+	ToDo := connect(import)
+	ToDo.string()
+	var name type[RegularExpression]
+	if listen=true 
+		{
+			var Action type(queries)
+			type queries struct {
+				ID     		int
+				Project    	string
+				Table []	bool
+			}
+			return=Message
+			
+		}
+	if listen=false
+		{
+			var Action type(null)
+			return=Message
+		}
+}
